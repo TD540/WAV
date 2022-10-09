f4f51ec21ea2e9d198f928ba1b55c7eb5e2c42a5
Thomas <thomas.decrick@icloud.com>
2022-10-09 22:59:11 +0200

    week schedule

--- WAV/WeekSchedule.swift
+++ WAV/WeekSchedule.swift
@@ -10,17 +10,50 @@ import WebView
 import WebKit
 
 struct WeekSchedule: View {
-    @StateObject var webViewStore = WebViewStore()
-
+    @StateObject var webViewStore: WebViewStore
+    let url = "https://wearevarious.com/week-schedule"
     var body: some View {
         WebView(webView: webViewStore.webView)
             .onAppear {
-                webViewStore.webView.load(URLRequest(url: URL(string: "https://wearevarious.com/week-schedule")!))
-
-                let css = "* { border: 1px solid red !important; }"
-                let js = "var style = document.createElement('style'); style.innerHTML = '\(css)'; document.head.appendChild(style);"
-                webViewStore.webView.evaluateJavaScript(js, completionHandler: nil)
+                webViewStore.webView.load(
+                    URLRequest(
+                        url: URL(string: url)!
+                    )
+                )
             }
+    }
+    init() {
+        /// Some CSS to hide everything but page content
+        let css = """
+                  body > :not(.box-wrapper),
+                  .menu-wrapper,
+                  .main-container > :not(.page-wrapper),
+                  #page-header {
+                    display: none !important
+                  }
+                  """
+        let js = """
+                 var style = document.createElement('style')
+                 style.innerHTML = '\(css)'
+                 document.head.appendChild(style)
+                 """
+        let script = WKUserScript(
+            source: js,
+            injectionTime: .atDocumentEnd,
+            forMainFrameOnly: true,
+            in: .defaultClient
+        )
+        let controller = WKUserContentController()
+        controller.addUserScript(script)
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController = controller
+        let store = WebViewStore(
+            webView: WKWebView(
+                frame: .zero,
+                configuration: configuration
+            )
+        )
+        _webViewStore = StateObject(wrappedValue: store)
     }
 }
 
