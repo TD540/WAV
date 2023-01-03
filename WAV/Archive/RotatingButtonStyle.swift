7bde942d2623c2197512e055cd406e8925a2dff3
Thomas <thomas.decrick@icloud.com>
2023-01-03 23:42:19 +0100

    RotatingButtonStyle view cleanup

--- WAV/Archive/RotatingButtonStyle.swift
+++ WAV/Archive/RotatingButtonStyle.swift
@@ -21,78 +21,76 @@ struct RotatingButtonStyle: ButtonStyle {
     var stoppingAnimation = Animation.linear(duration: 0)
 
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .modifier(
-                PausableRotationEffect(
-                    desiredAngle: desiredAngle,
-                    currentAngle: $currentAngle
+        VStack {
+            Text("currentAngle: \(currentAngle)")
+            configuration.label
+                .modifier(
+                    PausableRotationEffect(
+                        desiredAngle: desiredAngle,
+                        currentAngle: $currentAngle
+                    )
                 )
-            )
-            .onAppear {
-                if isRotating {
-                    withAnimation(foreverAnimation) {
-                        let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
-                        let angleDelta = CGFloat.pi * 2
-                        desiredAngle = startAngle + angleDelta
+                .onAppear {
+                    if isRotating {
+                        withAnimation(foreverAnimation) {
+                            let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
+                            let angleDelta = CGFloat.pi * 2
+                            desiredAngle = startAngle + angleDelta
+                        }
                     }
                 }
-            }
-            .onChange(of: isRotating, perform: { isRotatingChanged in
-                if isRotatingChanged == true {
-                    withAnimation(foreverAnimation) {
-                        let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
-                        let angleDelta = CGFloat.pi * 2
-                        desiredAngle = startAngle + angleDelta
+                .onChange(of: isRotating, perform: { isRotatingChanged in
+                    if isRotatingChanged == true {
+                        withAnimation(foreverAnimation) {
+                            let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
+                            let angleDelta = CGFloat.pi * 2
+                            desiredAngle = startAngle + angleDelta
+                        }
+                    } else {
+                        withAnimation(stoppingAnimation) {
+                            let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
+                            desiredAngle = startAngle
+                        }
                     }
-                } else {
-                    withAnimation(stoppingAnimation) {
-                        let startAngle = currentAngle.truncatingRemainder(dividingBy: CGFloat.pi * 2)
-                        desiredAngle = startAngle
-                    }
-                }
-            })
-
-    }
-}
-
-struct PausableRotationEffect: GeometryEffect {
-    @Binding var currentAngle: CGFloat
-    private var currentAngleValue: CGFloat = 0.0
-
-    var animatableData: CGFloat {
-        get { currentAngleValue }
-        set { currentAngleValue = newValue }
-    }
-
-    init(desiredAngle: CGFloat, currentAngle: Binding<CGFloat>) {
-        currentAngleValue = desiredAngle
-        _currentAngle = currentAngle
-    }
-
-    func effectValue(size: CGSize) -> ProjectionTransform {
-        DispatchQueue.main.async {
-            currentAngle = currentAngleValue
+                })
         }
-        let xOffset = size.width / 2
-        let yOffset = size.height / 2
-        let transform =
-            CGAffineTransform(translationX: xOffset, y: yOffset)
-            .rotated(by: currentAngleValue)
-            .translatedBy(x: -xOffset, y: -yOffset)
 
-        return ProjectionTransform(transform)
+    }
+
+    struct PausableRotationEffect: GeometryEffect {
+        @Binding var currentAngle: CGFloat
+        private var currentAngleValue: CGFloat = 0.0
+        var animatableData: CGFloat {
+            get { currentAngleValue }
+            set { currentAngleValue = newValue }
+        }
+        init(desiredAngle: CGFloat, currentAngle: Binding<CGFloat>) {
+            currentAngleValue = desiredAngle
+            _currentAngle = currentAngle
+        }
+        func effectValue(size: CGSize) -> ProjectionTransform {
+            DispatchQueue.main.async {
+                currentAngle = currentAngleValue
+            }
+            let xOffset = size.width / 2
+            let yOffset = size.height / 2
+            let transform =
+            CGAffineTransform(translationX: xOffset, y: yOffset)
+                .rotated(by: currentAngleValue)
+                .translatedBy(x: -xOffset, y: -yOffset)
+            return ProjectionTransform(transform)
+        }
     }
 }
 
-struct TestRotatingButtonStyle_Previews: PreviewProvider {
+struct RotatingButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
-        TestRotatingButtonStyle()
+         PreviewRotatingButtonStyle()
     }
-
-    struct TestRotatingButtonStyle: View {
+    struct PreviewRotatingButtonStyle: View {
         @State private var isPlaying: Bool = true
         var body: some View {
-            VStack {
+            TabView {
                 Button {
                     isPlaying.toggle()
                 } label: {
@@ -100,6 +98,13 @@ struct TestRotatingButtonStyle_Previews: PreviewProvider {
                         .font(.system(size: 200))
                 }
                 .buttonStyle(RotatingButtonStyle(isRotating: isPlaying))
+                .tabItem {
+                    Label("RotatingButtonStyle", systemImage: "opticaldisc")
+                }
+                Text("Tapped away, now tap back")
+                    .tabItem {
+                        Label("Tap me", systemImage: "ellipsis")
+                    }
             }
         }
     }
